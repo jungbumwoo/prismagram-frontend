02745945a4993c4708612665ce243c5fd6bda862
jungbum <jungbumwoo@naver.com>
2020-03-05 07:38:54 +0900

    0305

--- src/Routes/Auth.js
+++ src/Routes/Auth.js
@@ -5,10 +5,38 @@ const Wrapper = styled.div``;
 
 const Box = styled.div`
     ${props => props.theme.whiteBox}
+    border-radius:0px;
+    width: 350px;
+  `;
+  
+  const StateChanger = styled(Box)`
+    text-align: center;
+    padding: 20px 0px;
+  `;
+  
+  const Link = styled.span`
+    color: ${props => props.theme.blueColor};
+    cursor: pointer;
 `;
 
-export default ()=> {
+export default () => {
     const [action, setAction] = useState("logIn");
-
-    return action ==="logIn" ? "Log in" : "Sign up";
-};
+  
+    return (
+      <Wrapper>
+        <StateChanger>
+          {action === "logIn" ? (
+            <>
+              Don't have an account?{" "}
+              <Link onClick={() => setAction("signUp")}>Sign up</Link>
+            </>
+          ) : (
+            <>
+              Have an account?{" "}
+              <Link onClick={() => setAction("logIn")}>Log in</Link>
+            </>
+          )}
+        </StateChanger>
+      </Wrapper>
+    );
+  };
