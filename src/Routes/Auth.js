048f4ae9b33fc0eabd9a8d87b6289ccb592ee416
jungbum <jungbumwoo@naver.com>
2020-03-04 22:56:27 +0900

    0304_20:56

--- src/Routes/Auth.js
+++ src/Routes/Auth.js
@@ -1,1 +1,14 @@
-export default () => "Auth";
+import React, {useState} from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div``;
+
+const Box = styled.div`
+    ${props => props.theme.whiteBox}
+`;
+
+export default ()=> {
+    const [action, setAction] = useState("logIn");
+
+    return action ==="logIn" ? "Log in" : "Sign up";
+};
